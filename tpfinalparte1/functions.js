8f5dc183cabac2c95823405e52972ae5477e2ce9
alesiofiaccola <al3s1o2017@gmail.com>
2025-10-10 22:51:04 -0300

    Update functions.js

--- tpfinalparte1/functions.js
+++ tpfinalparte1/functions.js
@@ -1,119 +1,116 @@
 function mouseClicked(){
   sfx.amp(0.5);
   sfx.play();
- if(diapositiva === "inicio"){
-   if(tap(100, 400, 110, 50)){
-    diapositiva = "1";
+  print("Click detectado en:", mouseX, mouseY);
+  //inicio
+  if (diapositiva === "inicio" && tap(100, 400, 110, 50)){
+  diapositiva = "1";
   }
- }
- if(diapositiva === "1"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "2";
-   }
- }if(diapositiva === "2"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "a1";
-   }else if(tap(360, 400, 110, 50)){
-     diapositiva = "b1";
-   }
- }if(diapositiva === "a1"){
-   if(tap(100, 400, 110, 50)){
+  
+  //linea principal
+  else if (diapositiva === "1" && tap(100, 400, 110, 50)){
+  diapositiva = "2";
+  }
+  else if (diapositiva === "2" && tap(100, 400, 110, 50)) {
+    diapositiva = "a1";
+  }
+  else if (diapositiva === "2" && tap(360, 400, 110, 50)){ 
+    diapositiva = "b1";
+  }
+  
+  //rama a
+    else if (diapositiva === "a1" && tap(100, 400, 110, 50)){
      diapositiva = "a2";
-   }
- }if(diapositiva === "a2"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "a3";
-   }
- }if(diapositiva === "a3"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "c1";
-   }else if(tap(360, 400, 110, 50)){
-     diapositiva = "d1";
-   }
-  }if(diapositiva === "b1"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "b2";
-   }
- }if(diapositiva === "b2"){
-   if(tap(100, 400, 110, 50)){
+    }
+    else if (diapositiva === "a2" && tap(100, 400, 110, 50)) {
+      diapositiva = "a3";
+    }
+    else if (diapositiva === "a3" && tap(100, 400, 110, 50)) { 
+      diapositiva = "c1";
+    }
+    else if (diapositiva === "a3" && tap(360, 400, 110, 50)){ 
+    diapositiva = "d1";
+  }
+  
+  //rama b
+  else if (diapositiva === "b1" && tap(100, 400, 110, 50)){
+    diapositiva = "b2";
+  }
+  else if (diapositiva === "b2" && tap(100, 400, 110, 50)){
      diapositiva = "e1";
-   }else if(tap(360, 400, 110, 50)){
-     diapositiva = "f1";
-   }
- }if(diapositiva === "c1"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "c2";
-   }
- }if(diapositiva === "c2"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "d1";
-   }
- }if(diapositiva === "d1"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "d2";
-   }
- }if(diapositiva === "d2"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "d3";
-   }
- }if(diapositiva === "d3"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "d4";
-   }
- }if(diapositiva === "d4"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "g1";
-   }else if(tap(360, 400, 110, 50)){
-     diapositiva = "h1";
-   }
- }if(diapositiva === "e1"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "e2";
-   }
- }if(diapositiva === "e2"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "creditos";
-   }
- }if(diapositiva === "f1"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "f2";
-   }
- }if(diapositiva === "f2"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "creditos";
-   }
- }if(diapositiva === "g1"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "g2";
-   }
- }if(diapositiva === "g2"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "g3";
-   }
- }if(diapositiva === "g3"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "creditos";
-   }
- }if(diapositiva === "h1"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "h2";
-   }
- }if(diapositiva === "h2"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "creditos";
-   }
- }if(diapositiva === "h3"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "creditos";
-   }
- }if(diapositiva === "creditos"){
-   if(tap(100, 400, 110, 50)){
-     diapositiva = "inicio";
-   }
- }
+  }
+  else if (diapositiva === "b2" && tap(360, 400, 110, 50)) {
+    diapositiva = "f1";
+  }
+  
+  //rama c
+  else if (diapositiva === "c1" && tap(100, 400, 110, 50)){
+    diapositiva = "c2";
+  }
+  else if (diapositiva === "c2" && tap(100, 400, 110, 50)){
+  diapositiva = "d1";
+  }
+
+  //rama d
+  else if (diapositiva === "d1" && tap(100, 400, 110, 50)) {
+    diapositiva = "d2";
+  }
+  else if (diapositiva === "d2" &&tap(100, 400, 110, 50)){ 
+    diapositiva = "d3";
+  }
+  else if (diapositiva === "d3" && tap(100, 400, 110, 50)){
+    diapositiva = "d4";
+  }
+  else if (diapositiva === "d4" && tap(100, 400, 110, 50)){
+    diapositiva = "g1";   
+  } 
+  else if (diapositiva === "d4" && tap(360, 400, 110, 50)) {
+    diapositiva = "h1";
+  }
+  
+  //rama e
+  else if (diapositiva === "e1" && tap(100, 400, 110, 50)) {
+      diapositiva = "e2";
+  }
+  else if (diapositiva === "e2" && tap(100, 400, 110, 50)){
+    diapositiva = "creditos";
+  }
+  
+  //rama f
+   else if (diapositiva === "f1" && tap(100, 400, 110, 50)) {
+      diapositiva = "f2";
+  }
+  else if (diapositiva === "f2" && tap(100, 400, 110, 50)) {
+    diapositiva = "creditos";
+  }
+  
+  //rama g
+  else if (diapositiva === "g1" && tap(100, 400, 110, 50)) {
+      diapositiva = "g2";
+  }
+  else if (diapositiva === "g2" && tap(100, 400, 110, 50)) {
+    diapositiva = "g3";
+  }
+  else if (diapositiva === "g3" && tap(100, 400, 110, 50)) {
+    diapositiva = "creditos";
+  }
+  
+  
+  else if (diapositiva === "h1" && tap(100, 400, 110, 50)) {
+    diapositiva = "h2";
+  }
+  else if (diapositiva === "h2" && tap(100, 400, 110, 50)) {
+    diapositiva = "h3";
+  }
+   else if (diapositiva === "h3" && tap(100, 400, 110, 50)) {
+    diapositiva = "creditos";
+  }
+  else if ((diapositiva === "creditos" || diapositiva === "volver a iniciar") && tap(100, 400, 110, 50)) {
+    diapositiva = "inicio";
+  }
 }
 
-function botton(X, Y, W, H, text){
+function botton(X, Y, W, H, textoBoton){
   if(tap(X, Y, W,  H)){
     fill(255, 240, 0);
   }else{
@@ -122,9 +119,10 @@ function botton(X, Y, W, H, text){
   rect(X, Y, W, H);
   fill(0);
   textAlign(CENTER, CENTER);
-  text(text, X + W/2, Y + H/2);
+  text(textoBoton, X + W/2, Y + H/2);
 }
 
 function tap(x, y, w, h){
-  return mouseX > x && mouseX < x+w && mouseY > y && mouseY < y+h;
+  return mouseX >= x && mouseX <= x+w && mouseY >= y && mouseY <= y+h;
 }
+
